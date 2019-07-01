0896683a1d8a09821af3db1174ca252585ed215d
Sam Davis <sam@samgd.com>
2019-07-01 12:27:40 +0100

    Add build_vgg example

--- src/myrtlespeech/builders/vgg.py
+++ src/myrtlespeech/builders/vgg.py
@@ -21,6 +21,33 @@ def build_vgg(vgg_cfg: vgg_pb2.VGG, input_channels: int) -> torch.nn.Module:
 
     Returns:
         A :py:class:`torch.nn.Module` based on the config.
+
+    Example:
+
+        >>> from google.protobuf import text_format
+        >>> vgg_cfg_text = '''
+        ... vgg_config: A;
+        ... batch_norm: false;
+        ... use_output_from_block: 2;
+        ... '''
+        >>> vgg_cfg = text_format.Merge(
+        ...     vgg_cfg_text,
+        ...     vgg_pb2.VGG()
+        ... )
+        >>> build_vgg(vgg_cfg, input_channels=3)
+        Sequential(
+          (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+          (1): ReLU(inplace)
+          (2): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+          (3): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+          (4): ReLU(inplace)
+          (5): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+          (6): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+          (7): ReLU(inplace)
+          (8): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+          (9): ReLU(inplace)
+          (10): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
     """
     vgg_config_map = {
         index: letter for letter, index in vgg_pb2.VGG.VGG_CONFIG.items()
