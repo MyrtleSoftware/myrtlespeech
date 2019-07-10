5bd5617d54ad3782e7abe0c954e0c3d554e324d9
Sam Davis <sam@samgd.com>
2019-07-10 14:41:09 +0100

    Begin SpeechToText class implementation

---
+++ src/myrtlespeech/model/speech_to_text.py
@@ -0,0 +1,36 @@
+from typing import Callable, List, Union
+
+import torch
+
+from myrtlespeech.data.alphabet import Alphabet
+from myrtlespeech.model.encoder_decoder import EncoderDecoder
+from myrtlespeech.post_process.ctc_beam_decoder import CTCBeamDecoder
+from myrtlespeech.post_process.ctc_greedy_decoder import CTCGreedyDecoder
+
+
+class SpeechToText:
+    """A speech to text model.
+
+    .. todo::
+
+        Document this
+
+    Args:
+        encoder:
+        decoder:
+    """
+
+    def __init__(
+        self,
+        alphabet: Alphabet,
+        model: EncoderDecoder,
+        loss: torch.nn.CTCLoss,
+        pre_process_steps: List[Callable],
+        post_process: Union[None, CTCGreedyDecoder, CTCBeamDecoder],
+    ):
+        super().__init__()
+        self.alphabet = alphabet
+        self.model = model
+        self.loss = loss
+        self.pre_process_steps = pre_process_steps
+        self.post_process = post_process
---
+++ tests/protos/test_speech_to_text.py
@@ -0,0 +1,33 @@
+from typing import Union, Tuple, Dict
+
+import hypothesis.strategies as st
+
+from myrtlespeech.protos import encoder_decoder_pb2
+from tests.protos.utils import all_fields_set
+from tests.protos.test_decoder import decoders
+from tests.protos.test_encoder import encoders
+
+
+# Fixtures and Strategies -----------------------------------------------------
+
+
+@st.composite
+def encoder_decoders(
+    draw, return_kwargs: bool = False, valid_only: bool = False
+) -> Union[
+    st.SearchStrategy[encoder_decoder_pb2.EncoderDecoder],
+    st.SearchStrategy[Tuple[encoder_decoder_pb2.EncoderDecoder, Dict]],
+]:
+    """Returns a SearchStrategy for EncoderDecoder plus maybe the kwargs."""
+    kwargs = {}
+    kwargs["encoder"] = draw(encoders())
+    kwargs["decoder"] = draw(decoders(valid_only=valid_only))
+
+    # initialise encoder and return
+    all_fields_set(encoder_decoder_pb2.EncoderDecoder, kwargs)
+    encoder_decoder = encoder_decoder_pb2.EncoderDecoder(  # type: ignore
+        **kwargs
+    )
+    if not return_kwargs:
+        return encoder_decoder
+    return encoder_decoder, kwargs
