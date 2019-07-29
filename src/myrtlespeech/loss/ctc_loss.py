a03d45b473cbee61b647b610a61a86a11902e4cb
Sam Davis <sam@samgd.com>
2019-07-29 10:10:29 +0100

    Tests internal CTCLoss implementation matches PyTorch

---
+++ src/myrtlespeech/loss/ctc_loss.py
@@ -0,0 +1,90 @@
+from typing import Tuple, Union
+
+import torch
+
+
+class CTCLoss(torch.nn.Module):
+    """Wrapped :py:class:`torch.nn.CTCLoss` that applies LogSoftmax internally.
+
+    Args:
+        blank: Index of the blank label.
+
+        reduction: Specifies the reduction to apply to the output:
+
+            none:
+                No reduction will be applied.
+
+            mean:
+                The output losses will be divided by the target lengths and
+                then the mean over the batch is taken.
+
+            sum:
+                Sum all losses in a batch.
+
+        zero_infinity: Whether to zero infinite losses and the associated
+            gradients. (Infinite losses mainly occur when the inputs are too
+            short to be aligned to the targets)
+
+        dim: A dimension along which :py:class:`torch.nn.LogSoftmax` will be
+            computed.
+
+    Attributes:
+        log_softmax: A :py:class:`torch.nn.LogSoftmax` instance.
+
+        ctc_loss: A :py:class:`torch.nn.CTCLoss` instance.
+    """
+
+    def __init__(
+        self,
+        blank: int = 0,
+        reduction: str = "mean",
+        zero_infinity: bool = False,
+        dim: int = -1,
+    ):
+        super().__init__()
+        self.log_softmax = torch.nn.LogSoftmax(dim=dim)
+        self.ctc_loss = torch.nn.CTCLoss(
+            blank=blank, reduction=reduction, zero_infinity=zero_infinity
+        )
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        targets: torch.Tensor,
+        input_lengths: Union[Tuple, torch.Tensor],
+        target_lengths: Union[Tuple, torch.Tensor],
+    ) -> torch.Tensor:
+        """Computes CTC loss.
+
+        Much of the following documentation is from
+        :py:class:`torch.nn.CTCLoss`.
+
+        Args:
+            inputs: Unnormalized network :py:class:`torch.Tensor` outputs of
+                size ``(max_seq_len, batch, features)``.
+
+            targets: :py:class:`torch.Tensor` where each element in the target
+                sequence is a class index. Target indices cannot be the blank
+                index.
+
+                Must have size ``(batch, max_seq_len)`` or
+                ``sum(target_lengths)``. In the former form each target
+                sequence is padded to the length of the longest sequence and
+                stacked.
+
+            input_lengths: Length of the inputs (each must be ``<= batch``).
+                Lengths are specified for each sequence to achieve masking
+                under the assumption that sequences are padded to equal
+                lengths.
+
+            target_lengths: Lengths of the targets. Lengths are specified for
+                each sequence to achieve masking under the assumption that
+                sequences are padded to equal lengths.
+        """
+        log_probs = self.log_softmax(inputs)
+        return self.ctc_loss(
+            log_probs=log_probs,
+            targets=targets,
+            input_lengths=input_lengths,
+            target_lengths=target_lengths,
+        )
---
+++ tests/loss/test_ctc_loss.py
@@ -0,0 +1,109 @@
+from typing import Dict
+
+import hypothesis.strategies as st
+import numpy as np
+import torch
+from hypothesis import given
+
+from myrtlespeech.loss.ctc_loss import CTCLoss
+from tests.utils.utils import arrays, tensors
+
+
+# Fixtures and Strategies -----------------------------------------------------
+
+
+@st.composite
+def ctc_loss_arguments(draw) -> st.SearchStrategy[Dict]:
+    """Generates args for class constructor and forward."""
+    ret_args = {}
+
+    # generate input tensor
+    ret_args["inputs"] = draw(
+        tensors(min_n_dims=3, max_n_dims=3, min_dim_size=2, max_dim_size=32)
+    )
+    # get shapes, convert to Python ints for Hypothesis
+    max_seq_len, batch, features = ret_args["inputs"].size()
+    max_seq_len = int(max_seq_len)
+    batch = int(batch)
+    features = int(features)
+
+    # generate CTCLoss arguments
+    ret_args["blank"] = draw(st.integers(min_value=0, max_value=features - 1))
+    ret_args["reduction"] = draw(st.sampled_from(["none", "mean", "sum"]))
+    ret_args["zero_infinity"] = draw(st.booleans())
+
+    # generate remaining CTCLoss.forward arguments
+    ret_args["targets"] = torch.tensor(
+        draw(
+            arrays(
+                shape=(batch, max_seq_len),
+                dtype=np.int32,
+                elements=st.integers(
+                    min_value=0, max_value=features - 1
+                ).filter(lambda x: x != ret_args["blank"]),
+            )
+        ),
+        requires_grad=False,
+    )
+
+    ret_args["input_lengths"] = torch.tensor(
+        draw(
+            arrays(
+                shape=(batch,),
+                dtype=np.int32,
+                elements=st.integers(min_value=1, max_value=max_seq_len),
+            )
+        ),
+        requires_grad=False,
+    )
+
+    target_lengths = []
+    for length in ret_args["input_lengths"]:
+        # ensure CTC requirement that target length <= input length
+        target_lengths.append(draw(st.integers(1, int(length))))
+    ret_args["target_lengths"] = torch.tensor(
+        target_lengths, dtype=torch.int32, requires_grad=False
+    )
+
+    return ret_args
+
+
+# Tests -----------------------------------------------------------------------
+
+
+@given(args=ctc_loss_arguments())
+def test_ctc_loss_matches_torch(args) -> None:
+    """Ensures CTCLoss matches torch CTCLoss(LogSoftmax(...), ...)."""
+    myrtle_ctc_loss = CTCLoss(
+        blank=args["blank"],
+        reduction=args["reduction"],
+        zero_infinity=args["zero_infinity"],
+    )
+
+    torch_log_softmax = torch.nn.LogSoftmax(dim=-1)
+    torch_ctc_loss = torch.nn.CTCLoss(
+        blank=args["blank"],
+        reduction=args["reduction"],
+        zero_infinity=args["zero_infinity"],
+    )
+
+    actual = myrtle_ctc_loss(
+        inputs=args["inputs"],
+        targets=args["targets"],
+        input_lengths=args["input_lengths"],
+        target_lengths=args["target_lengths"],
+    )
+
+    expected = torch_ctc_loss(
+        log_probs=torch_log_softmax(args["inputs"]),
+        targets=args["targets"],
+        input_lengths=args["input_lengths"],
+        target_lengths=args["target_lengths"],
+    )
+
+    assert isinstance(actual, torch.Tensor)
+    assert isinstance(expected, torch.Tensor)
+
+    assert actual.size() == expected.size()
+
+    assert torch.allclose(actual, expected)
