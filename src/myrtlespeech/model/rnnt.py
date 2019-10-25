fb7c14317e40bdc34909ba9292256a80a0d4a6fc
julianmack <julianmack@hotmail.co.uk>
2019-10-25 18:38:53 +0100

    Updated rnnt docstrings and .joint() args ordering

--- src/myrtlespeech/model/rnnt.py
+++ src/myrtlespeech/model/rnnt.py
@@ -32,17 +32,17 @@ class RNNT(torch.nn.Module):
 
         embedding: A :py:class:`torch.nn.Module` which is an embedding lookup for
             targets (eg graphemes, wordpieces) which accepts a :py:`torch.Tensor`
-            as input of size ``[max_output_seq_len, batch, in_features]``
+            as input of size ``[batch, max_output_seq_len]``
 
         dec_rnn: A :py:class:`torch.nn.Module` containing the recurrent part of
             the RNN-T prediction.
 
             Must accept as input a tuple where the first element is the prediction
-            network input (a :py:`torch.Tensor`) with size ``[max_output_seq_len,
-            batch, in_features]`` and the second element is a
+            network input (a :py:`torch.Tensor`) with size ``[batch, max_output_seq_len + 1,
+            in_features]`` and the second element is a
             :py:class:`torch.Tensor` of size ``[batch]`` where each entry
-            representfc_in_dims the sequence length of the corresponding *input*
-            sequence to the rnn.
+            represent the sequence length of the corresponding *input*
+            sequence to the rnn. *Note: this must be a `batch_first` rnn.*
 
             It must return a tuple where the first element is the result after
             applying the module to the input. It must have size
@@ -52,10 +52,29 @@ class RNNT(torch.nn.Module):
             corresponding *output* sequence. These may be different than the
             input sequence lengths due to downsampling in the encoder.
 
-    Returns:
-        The output of ``rnn2`` if it is not None, else the output of ``rnn1``.
-        The first element of the tensor output is transposed so that it is
-        of size ``[batch, new_seq_len, out_features]``
+        fully_connected: A :py:class:`torch.nn.Module` containing the fully
+            connected part of the RNNT joint network.
+
+            Must accept as input a tuple where the first element is
+            a :py:class:`torch.Tensor` with size ``[batch,
+            encoder_out_seq_len, dec_rnn_out_seq_len, hidden_dim]`` where `hidden_dim`
+            is the the concatenation of the `encoder` and `dec_rnn` outputs along
+            the hidden dimension axis. The second element is and the
+            input (a :py:class:`torch.Tensor`) with size ``[batch,
+            max_fc_in_seq_len, max_fc_in_features]`` and the second element is
+            a :py:class:`torch.Tensor` of size ``[batch]`` where each entry
+            represents the sequence length of the corresponding *input*
+            sequence to the fully connected layer(s). ``max_fc_in_seq_len`` and
+            ``max_fc_in_features`` will either be ``max_rnn_seq_len`` and
+            ``rnn_features`` or ``max_la_seq_len`` and ``la_features``
+            depending on whether lookahead is :py:data:`None`.
+
+            It must return a tuple where the first element is the result after
+            applying the module to the previous layers output. It must have
+            size ``[batch, max_out_seq_len, out_features]``. The second element
+            is returned unchanged but in this context should be a Tuple of two
+            :py:class:`torch.Tensor`s both of size ``[batch]`` that contain the *output* lengths of a) the audio features
+            inputs and b) the target sequences.
 
     """
 
@@ -63,91 +82,148 @@ class RNNT(torch.nn.Module):
         super().__init__()
 
         assert (
-            dec_rnn.batch_first == False
-        ), "dec_rnn should not be a batch_first rnn"
+            dec_rnn.batch_first == True
+        ), "dec_rnn should be a batch_first rnn"
         self.encode = encoder
         self.embedding = embedding
         self.dec_rnn = dec_rnn
-        self.fc = fully_connected
+        self.fully_connected = fully_connected
 
         self.use_cuda = torch.cuda.is_available()
 
     def forward(
         self,
-        inp: Tuple[
+        x: Tuple[
             Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor, torch.Tensor]
         ],
     ) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
         """Returns the result of applying the RNN-T network to the input audio
-        features ``inp[0][0]`` and target labels ``inp[0][1]``.
+        features ``x[0][0]`` and target labels ``x[0][1]``.
 
         All inputs are moved to the GPU with :py:meth:`torch.nn.Module.cuda` if
         :py:func:`torch.cuda.is_available` was :py:data:`True` on
         initialisation.
 
         Args:
-            inp: Input for the RNNT network. TODO
-
+            x: A Tuple ``(x[0], x[1])``. ``x[0]`` is input to the network and is
+                a Tuple ``x[0] = (x[0][0], x[0][1])`` where both elements are
+                :py:class:`torch.Tensor`s. ``x[0][0]`` is the audio feature input
+                with size ``[max_seq_len, batch, in_features]`` while ``x[0][1]`` is
+                the target label tensor of size ``[batch, max_label_length]``.
+                ``x[1]`` is a Tuple of two :py:class:`torch.Tensor`s both of
+                size ``[batch]`` that contain the *input* lengths of a) the audio feature
+                inputs ``x[1][0]`` and b) the target sequences ``x[1][1]``.
         Returns:
-            TODO
+            A Tuple where the first element is the output of the RNNT network: a
+                :py:class:`torch.Tensor` with size ``[batch, max_seq_len,
+                max_label_length + 1, vocab_size + 1]``, and the second element
+                is a Tuple of two :py:class:`torch.Tensor`s both of
+                size ``[batch]`` that contain the *output* lengths of a) the audio features
+                inputs and b) the target sequences. These may be of different
+                lengths than the inputs as a result of downsampling.
+
             """
 
-        self._certify_inputs_forward(inp)
-        (x, y), (x_lens, y_lens) = inp
+        self._certify_inputs_forward(x)
+
+        # Now use 'x_inp' to refer to audio features
+        (x_inp, y), (x_lens, y_lens) = x
 
         if self.is_cuda:
-            x = x.cuda()
+            x_inp = x_inp.cuda()
             x_lens = x_lens.cuda()
             y = y.cuda()
             y_lens = y_lens.cuda()
-            inp = (x, y), (y, y_lens)
         # TODO - convert to half precision here?
 
-        X_ = (x, x_lens)
+        X_ = (x_inp, x_lens)
         Y_ = (y, y_lens)
 
         f = self.encode(X_)  # f[0] = (T, B, H1)
         g = self.prediction(Y_)  # g[0] = (U, B, H2)
-        return self.joint(f, g)
-
-    def prediction(self, y):
-        y_vals, y_lens = y
-
-        y_vals = self.embedding(y_vals)
-        y_vals = self._append_SOS(y_vals)
-        return self.dec_rnn((y_vals, y_lens))
-
-    def joint(self, f, g):
-        T, B1, H1 = f[0].shape
-        U_, B2, H2 = g[0].shape
-
-        assert (
-            B1 == B2
-        ), "Batch size from prediction network and transcription must be equal"
 
         f_inp = f[0]
         g_inp = g[0]
         seq_lengths = (f[1], g[1])  # (time_lens, label_lens)
 
-        f_inp = f_inp.transpose(1, 0)  # B1, T, H1
-        f_inp = f_inp.unsqueeze(dim=2)  # (B, T, 1, H)
-        f_inp = f_inp.expand((B1, T, U_, H1)).contiguous()
+        return self.joint(((f_inp, g_inp), seq_lengths))
 
-        g_inp = g_inp.transpose(1, 0)  # B1, T, H1
-        g_inp = g_inp.unsqueeze(dim=2)  # (B, T, 1, H)
-        g_inp = g_inp.expand((B1, T, U_, H2)).contiguous()
+    def prediction(
+        self, x: Tuple[torch.Tensor, torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Returns the result of applying the RNN-T prediction network to the
+        target labels ``x[0][1]``. This function is only appropriate
+        during training (when the ground-truth labels are available).
+        TODO - reference where the inference function is found.
+
+        All inputs are moved to the GPU with :py:meth:`torch.nn.Module.cuda` if
+        :py:func:`torch.cuda.is_available` was :py:data:`True` on
+        initialisation.
+
+        Args:
+            x: A Tuple where the first element is the target label tensor of
+                size ``[batch, max_label_length]`` and the second is a
+                :py:class:`torch.Tensor` of size ``[batch]`` that contain the
+                *input* lengths of these target label sequences.
+
+        Returns:
+            Output from ``dec_rnn``. See initialisation docstring.
+        """
+
+        y_vals, y_lens = x
+
+        y_vals = self.embedding(y_vals)
+        y_vals = self._append_SOS(y_vals)
+        return self.dec_rnn((y_vals, y_lens))
+
+    def joint(
+        self,
+        x: Tuple[
+            Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor, torch.Tensor]
+        ],
+    ) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        """
+        Returns the result of applying the RNN-T joint network to the encoder
+        hidden state audio features ``x[0][0]`` and the prediction network
+        target label hidden state ``x[0][1]``.
+
+        Args:
+            x: A Tuple ``(x[0], x[1])``. ``x[0][0]`` is the first element of the
+                encoder network output (i.e. `.encode(...)[0]` - see
+                :py:class:`.RNNTEncoder` docstring). ``x[0][1]`` is the first element of the
+                prediction network output (i.e. `.prediction(...)[0]` - see prediction docstring).
+                ``x[1]`` is a Tuple of two :py:class:`torch.Tensor`s both of
+                size ``[batch]`` that contain the *input* lengths of a) the audio feature
+                inputs ``x[1][0]`` and b) the target sequences ``x[1][1]``.
+        Returns:
+            The output of the :py:class:`.RNNT` network. See initialisation docstring.
+        """
+        (f, g), seq_lengths = x
+
+        T, B1, H1 = f.shape
+        B2, U_, H2 = g.shape
+
+        assert (
+            B1 == B2
+        ), "Batch size from prediction network and transcription must be equal"
+
+        f = f.transpose(1, 0)  # B1, T, H1
+        f = f.unsqueeze(dim=2)  # (B, T, 1, H)
+        f = f.expand((B1, T, U_, H1)).contiguous()
+
+        g = g.unsqueeze(dim=1)  # (B, 1, U_, H)
+        g = g.expand((B1, T, U_, H2)).contiguous()
 
         joint_inp = torch.cat([f, g], dim=3).contiguous()  # (B, T, U_, H1 + H2)
 
-        return self.fc((joint_inp, seq_lengths))
+        return self.fully_connected((joint_inp, seq_lengths))
 
     def _append_SOS(self, y):
-        """Appends the SOS token (all zeros)
-        to the start of the target tensor"""
+        """Appends the SOS token (all zeros) to the start of the target tensor"""
 
-        U, B, H = y.shape
+        B, U, H = y.shape
         # preprend blank
-        start = torch.ones((1, B, H)) * 0
+        start = torch.zeros((B, 1, H))
         y = torch.cat([start, y], dim=1)  # (B, U + 1, H)
         y = y.contiguous()
         return y
