28836d95957f75bba794188decbc6e8a84e8fd41
julianmack <julianmack@hotmail.co.uk>
2019-11-14 11:24:21 +0000

    Refactored train loop to two functions

--- src/myrtlespeech/run/train.py
+++ src/myrtlespeech/run/train.py
@@ -47,44 +47,48 @@ def fit(
 
         for stage in stages:
             is_training = stage == Stage.TRAIN
-            seq_to_seq.train(mode=is_training)
-            cb_handler.train(mode=is_training)
-
-            cb_handler.on_epoch_begin()
-
-            with ExitStack() as stack:
-                if not is_training:
-                    stack.enter_context(torch.no_grad())
-
-                loader = train_loader if is_training else eval_loader
-                for x, y in loader:
-                    # model
-                    x, y = cb_handler.on_batch_begin(x, y)
-                    out = seq_to_seq.model(x)
-
-                    # loss
-                    loss_out, loss_y = cb_handler.on_loss_begin(out, y)
-                    loss = seq_to_seq.loss(loss_out, loss_y)
-                    loss, skip_bwd = cb_handler.on_backward_begin(loss)
-
-                    # optim
-                    if is_training:
-                        if not skip_bwd:
-                            loss.backward()
-
-                        if seq_to_seq.optim is not None:
-                            if not cb_handler.on_backward_end():
-                                seq_to_seq.optim.step()
-
-                            if not cb_handler.on_step_end():
-                                seq_to_seq.optim.zero_grad()
-
-                    if cb_handler.on_batch_end():
-                        break
-
-                    del x, y, out, loss, loss_out, loss_y
-            if cb_handler.on_epoch_end():
+            loader = train_loader if is_training else eval_loader
+            if run_stage(seq_to_seq, cb_handler, loader, is_training):
                 break
 
     cb_handler.on_train_end()
     # sphinx-doc-end-before
+
+
+def run_stage(seq_to_seq, cb_handler, loader, is_training):
+    seq_to_seq.train(mode=is_training)
+    cb_handler.train(mode=is_training)
+
+    cb_handler.on_epoch_begin()
+
+    with ExitStack() as stack:
+        if not is_training:
+            stack.enter_context(torch.no_grad())
+
+        for x, y in loader:
+
+            x, y = cb_handler.on_batch_begin(x, y)
+            out = seq_to_seq.model(x)
+
+            # loss
+            loss_out, loss_y = cb_handler.on_loss_begin(out, y)
+            loss = seq_to_seq.loss(loss_out, loss_y)
+            loss, skip_bwd = cb_handler.on_backward_begin(loss)
+
+            # optim
+            if is_training:
+                if not skip_bwd:
+                    loss.backward()
+
+                if seq_to_seq.optim is not None:
+                    if not cb_handler.on_backward_end():
+                        seq_to_seq.optim.step()
+
+                    if not cb_handler.on_step_end():
+                        seq_to_seq.optim.zero_grad()
+            if cb_handler.on_batch_end():
+                break
+
+            del x, y, out, loss, loss_out, loss_y
+
+    return cb_handler.on_epoch_end()
