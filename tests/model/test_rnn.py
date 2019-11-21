d7c6f905add23b624def8a8c17d85db1d6d36281
julianmack <julianmack@hotmail.co.uk>
2019-11-21 15:45:12 +0000

    Added rnn tests. All passing.

--- tests/model/test_rnn.py
+++ tests/model/test_rnn.py
@@ -206,9 +206,9 @@ def test_rnn_forward_pass_correct_shapes_returned(
 
 @given(
     rnn_type=rnn_types(),
-    input_size=st.integers(min_value=1, max_value=128),
-    hidden_size=st.integers(min_value=1, max_value=128),
-    num_layers=st.integers(min_value=1, max_value=4),
+    input_size=st.integers(min_value=1, max_value=8),
+    hidden_size=st.integers(min_value=1, max_value=8),
+    num_layers=st.integers(min_value=1, max_value=3),
     bias=st.booleans(),
     dropout=st.floats(min_value=0.0, max_value=1.0, allow_nan=False),
     bidirectional=st.booleans(),
@@ -216,6 +216,8 @@ def test_rnn_forward_pass_correct_shapes_returned(
         st.none(), st.floats(min_value=-10.0, max_value=10.0, allow_nan=False)
     ),
     batch_first=st.booleans(),
+    batch_size=st.integers(min_value=1, max_value=4),
+    max_seq_len=st.integers(min_value=1, max_value=8),
 )
 @settings(deadline=4000)
 def test_rnn_forward_pass_no_hidden(
@@ -228,6 +230,8 @@ def test_rnn_forward_pass_no_hidden(
     bidirectional: bool,
     forget_gate_bias: float,
     batch_first: bool,
+    batch_size: int,
+    max_seq_len: int,
 ) -> None:
     """Tests forward rnn pass with no hidden state passed"""
 
@@ -238,21 +242,136 @@ def test_rnn_forward_pass_no_hidden(
         num_layers=num_layers,
         bias=bias,
         # warning raised when num_layers > 1 and dropout != 0.0 as this option
-        # set does not make sense
+        # set does not make sense:
         dropout=0.0 if num_layers == 1 else dropout,
         bidirectional=bidirectional,
         forget_gate_bias=forget_gate_bias,
         batch_first=batch_first,
     )
+    num_directions = 1 + int(bidirectional)
+    tensor = torch.empty(
+        [max_seq_len, batch_size, input_size], requires_grad=False
+    ).normal_()
 
-    # TODO - test_forward_pass
-    # #sample T, B, F
-    # if batch_first:
-    #     in_shape = (B, T, F)
-    # else:
-    #     in_shape = (T, B, F)
-    #
-    # x = torch.ones(*in_shape)
+    in_seq_lens = torch.randint(
+        low=1,
+        high=max_seq_len + 1,
+        size=[batch_size],
+        dtype=torch.int32,
+        requires_grad=False,
+    )
+    out_shape = hidden_size * num_directions
+    exp_shape = (max_seq_len, batch_size, out_shape)
+
+    if batch_first:
+        tensor = tensor.transpose(1, 0)
+        exp_shape = (batch_size, max_seq_len, out_shape)
+    res = rnn((tensor, in_seq_lens))
+
+    assert isinstance(res, tuple) and len(res) == 2
+    assert isinstance(res[0], torch.Tensor)
+    assert res[0].shape == exp_shape
+    assert torch.allclose(res[1].int(), in_seq_lens)
+
+
+@given(
+    rnn_type=rnn_types(),
+    input_size=st.integers(min_value=1, max_value=4),
+    hidden_size=st.integers(min_value=1, max_value=4),
+    num_layers=st.integers(min_value=1, max_value=2),
+    bias=st.booleans(),
+    dropout=st.floats(min_value=0.0, max_value=1.0, allow_nan=False),
+    bidirectional=st.booleans(),
+    forget_gate_bias=st.one_of(
+        st.none(), st.floats(min_value=-10.0, max_value=10.0, allow_nan=False)
+    ),
+    batch_first=st.booleans(),
+    batch_size=st.integers(min_value=1, max_value=2),
+    max_seq_len=st.integers(min_value=1, max_value=4),
+    hidden_is_none=st.booleans(),
+)
+@settings(deadline=4000)
+def test_rnn_forward_pass_hidden_passed(
+    rnn_type: RNNType,
+    input_size: int,
+    hidden_size: int,
+    num_layers: int,
+    bias: int,
+    dropout: float,
+    bidirectional: bool,
+    forget_gate_bias: float,
+    batch_first: bool,
+    batch_size: int,
+    max_seq_len: int,
+    hidden_is_none: bool,
+) -> None:
+    """Tests forward rnn pass.
+
+    Test both hidden=None and hidden=(h_0, c_0)."""
+
+    rnn = RNN(
+        rnn_type=rnn_type,
+        input_size=input_size,
+        hidden_size=hidden_size,
+        num_layers=num_layers,
+        bias=bias,
+        # warning raised when num_layers > 1 and dropout != 0.0 as this option
+        # set does not make senseno:
+        dropout=0.0 if num_layers == 1 else dropout,
+        bidirectional=bidirectional,
+        forget_gate_bias=forget_gate_bias,
+        batch_first=batch_first,
+    )
+    num_directions = 1 + int(bidirectional)
+    tensor = torch.empty(
+        [max_seq_len, batch_size, input_size], requires_grad=False
+    ).normal_()
+
+    in_seq_lens = torch.randint(
+        low=1,
+        high=max_seq_len + 1,
+        size=[batch_size],
+        dtype=torch.int32,
+        requires_grad=False,
+    )
+    # hidden state:
+    hidden = None
+    if not hidden_is_none:
+        h_0 = torch.empty(
+            [num_layers * num_directions, batch_size, hidden_size],
+            requires_grad=False,
+        ).normal_()
+        if rnn_type in [RNNType.BASIC_RNN, RNNType.GRU]:
+            hidden = h_0
+        elif rnn_type == RNNType.LSTM:
+            c_0 = h_0  # i.e. same dimensions
+            hidden = h_0, c_0
+
+    out_shape = hidden_size * num_directions
+    exp_shape = (max_seq_len, batch_size, out_shape)
+    if batch_first:
+        tensor = tensor.transpose(1, 0)
+        exp_shape = (batch_size, max_seq_len, out_shape)
+
+    res = rnn(((tensor, hidden), in_seq_lens))
+
+    assert isinstance(res, tuple) and len(res) == 2
+    assert isinstance(res[0], tuple)
+    assert res[0][0].shape == exp_shape
+    assert torch.allclose(res[1].int(), in_seq_lens)
+
+    # check hidden
+    hid = res[0][1]
+    if rnn_type in [RNNType.BASIC_RNN, RNNType.GRU]:
+        assert isinstance(hid, torch.Tensor)
+    elif rnn_type == RNNType.LSTM:
+        assert isinstance(hid, tuple)
+        assert len(hid) == 2
+        assert isinstance(hid[0], torch.Tensor) and isinstance(
+            hid[1], torch.Tensor
+        )
+    else:
+        raise TypeError(f"RNNType={rnn_type} was not expected")
 
 
 @given(rnn_type=st.integers(min_value=100, max_value=300))
