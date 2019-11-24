d3491c5ee6a9430ee3f8eece32c95ead4783c6cb
Sam Davis <sam@samgd.com>
2019-11-24 14:53:55 +0000

    CommonVoice add VERSION, documentation, comments

--- src/myrtlespeech/data/dataset/commonvoice.py
+++ src/myrtlespeech/data/dataset/commonvoice.py
@@ -15,23 +15,38 @@ from torch.utils.data import Dataset
 
 
 class CommonVoice(Dataset):
-    """CommonVoice Dataset - https://voice.mozilla.org/en
+    f"""`CommonVoice Dataset <https://voice.mozilla.org/>`_.
 
     Args:
         root: Root directory of the dataset. This should contain the
-            ``CommonVoice`` directory which itself contains a clips directory
-            containing all voice samples and a tsv file for each subset with
-            paths to samples and their transcription
+            ``CommonVoice`` directory with a tsv file for each subset that
+            contains paths to samples and transcriptions. There should also be
+            a ``clips`` directory containing all the audio files. e.g.
+
+            .. code-block::
+
+                CommonVoice/
+                    dev.tsv
+                    invalidated.tsv
+                    other.tsv
+                    test.tsv
+                    train.tsv
+                    validated.tsv
+                    clips/
+                       common_voice_en_1.mp3
+                       common_voice_en_10.mp3
+                       common_voice_en_100.mp3
+                       ...
 
         subsets: List of subsets to create the dataset from.  Subset names must
             be one of:
 
-                ``'other'``
-                ``'train'``
                 ``'dev'``
                 ``'invalidated'``
+                ``'other'``
                 ``'test'``
-                ``'validated``
+                ``'train'``
+                ``'validated'``
 
         audio_transform: A function that returns a transformed piece of audio
             data.
@@ -50,7 +65,12 @@ class CommonVoice(Dataset):
 
         max_duration: All samples with duration (in seconds) greater than this
             will be dropped.
+
+    Attributes:
+        VERSION: The CommonVoice version string.
     """
+
+    VERSION: str = "en_1087h_2019-06-12"
 
     base_dir = "CommonVoice"
     subset_hashes = {
@@ -102,7 +122,7 @@ class CommonVoice(Dataset):
         samples)``.
 
         Args:
-            index: Index.
+            index: Index of sample to return.
 
         Returns:
             Tuple of ``(audiodata, target)`` where audiodata is the possibly
@@ -110,10 +130,6 @@ class CommonVoice(Dataset):
             target transcription.
         """
         path = self.paths[index]
-
-        # Downsample to 16KHz sample rate for consistency with other data sets
-        # Not using torchaudio and SoxEffectChain for this since it is not
-        # thread safe
         audio, rate = torchaudio.load(path)
 
         assert rate == 16000, f"{path} sample rate == {rate} != 16000"
@@ -143,16 +159,16 @@ class CommonVoice(Dataset):
         return subsets
 
     def download(self) -> None:
-        """Extracts data set unless already cached.
+        """Extracts dataset unless already cached.
 
-        If the clips directory and all required data set files already exist,
-        this function becomes a noop
+        If the ``clips`` directory and all required dataset files already
+        exist, this function becomes a noop.
 
-        If the clips directory or one of the data set files are missing then
-        the function attempts to find and extract the zipped data set file
+        If the ``clips`` directory or one of the dataset files are missing then
+        the function attempts to find and extract the zipped dataset file.
 
-        If this file is not found, it prompts the user to download the data set
-        from Mozilla's website
+        If this file is not found, it prompts the user to download the dataset
+        from Mozilla's website.
         """
         os.makedirs(os.path.join(self.root, self.base_dir), exist_ok=True)
 
@@ -172,11 +188,11 @@ class CommonVoice(Dataset):
         if not os.path.isfile(path):
             # Get user to manually download file since Mozilla wants to collect
             # email addresses and agree to not identify speakers when users do
-            # this In the future, we may want to host this ourselves since the
-            # data set is licensed with CC0
+            # this. In the future, we may want to host this ourselves since the
+            # dataset is licensed with CC0.
             raise NotImplementedError(
-                "Please download the data set from "
-                f"https://voice.mozilla.org/en and place it in {self.root}"
+                f"Download dataset version `{CommonVoice.VERSION}` from "
+                f"https://voice.mozilla.org/en and place it in {self.root}."
             )
 
         if utils.checksum_file(path, "md5") != self.archive_hash:
@@ -249,7 +265,9 @@ class CommonVoice(Dataset):
         down_file = file[:-3] + "wav"
         down_path = os.path.join(root, "clips_down", down_file)
         if not os.path.isfile(down_path):
-            # Downsample file to 16KHz
+            # Downsample to 16KHz sample rate for consistency with other data
+            # sets. Not using torchaudio and SoxEffectChain for this since it
+            # is not thread safe
             audio = pydub.AudioSegment.from_mp3(path)
             audio = audio.set_frame_rate(16000)
 
