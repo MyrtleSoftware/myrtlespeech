318763d6417dd7d8e805b1b34ba50795564f2ec5
julianmack <julianmack@hotmail.co.uk>
2019-11-26 17:14:09 +0000

    Replaced dropout in fc proto with FloatValue

--- tests/protos/test_fully_connected.py
+++ tests/protos/test_fully_connected.py
@@ -41,7 +41,9 @@ def fully_connecteds(
             )
         )
 
-    all_fields_set(fully_connected_pb2.FullyConnected, kwargs)
+    all_fields_set(
+        fully_connected_pb2.FullyConnected, kwargs, to_ignore=to_ignore
+    )
     fc = fully_connected_pb2.FullyConnected(**kwargs)
     if not return_kwargs:
         return fc
