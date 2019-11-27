697f867fdbac9be317b53b6b9ca89176b9c01b59
julianmack <julianmack@hotmail.co.uk>
2019-11-27 10:03:59 +0000

    Removed Time Reduction functionality from Transducer encoder and updated tests/protos etc

--- src/myrtlespeech/builders/rnn_t.py
+++ src/myrtlespeech/builders/rnn_t.py
@@ -6,10 +6,8 @@ from myrtlespeech.builders.fully_connected import (
     build as build_fully_connected,
 )
 from myrtlespeech.builders.rnn import build as build_rnn
-from myrtlespeech.data.stack import StackTime
 from myrtlespeech.model.rnn_t import RNNT
 from myrtlespeech.model.rnn_t import RNNTEncoder
-from myrtlespeech.model.utils import Lambda
 from myrtlespeech.protos import rnn_t_encoder_pb2
 from myrtlespeech.protos import rnn_t_pb2
 from torch import nn
@@ -270,31 +268,7 @@ def build_rnnt_enc(
         )
         input_features = out_fc1
 
-    rnn1, rnn1_out_features = build_rnn(rnn_t_enc.rnn1, input_features)
-
-    if rnn_t_enc.time_reduction_factor == 0:  # default value (i.e. not set)
-        assert rnn_t_enc.HasField("rnn2") is False
-
-        time_reducer, rnn2 = None, None
-        reduction = 1
-        rnn_out_features = rnn1_out_features
-
-    else:
-        time_reduction_factor = rnn_t_enc.time_reduction_factor
-
-        assert (
-            time_reduction_factor > 1
-        ), "time_reduction_factor must be an integer > 1 but \
-        equals {time_reduction_factor}"
-
-        reduction = rnn_t_enc.time_reduction_factor
-
-        time_reducer = Lambda(StackTime(reduction))
-
-        rnnt_input_features = rnn1_out_features * reduction
-
-        rnn2, rnn_out_features = build_rnn(rnn_t_enc.rnn2, rnnt_input_features)
-
+    rnn1, rnn_out_features = build_rnn(rnn_t_enc.rnn1, input_features)
     # maybe add fc2:
     fc2: Optional[torch.nn.Module] = None
     if rnn_t_enc.HasField("fc2"):
@@ -311,13 +285,6 @@ def build_rnnt_enc(
     else:
         output_features = rnn_out_features
 
-    encoder = RNNTEncoder(
-        rnn1=rnn1,
-        fc1=fc1,
-        time_reducer=time_reducer,
-        time_reduction_factor=reduction,
-        rnn2=rnn2,
-        fc2=fc2,
-    )
+    encoder = RNNTEncoder(rnn1=rnn1, fc1=fc1, fc2=fc2)
 
     return encoder, output_features
