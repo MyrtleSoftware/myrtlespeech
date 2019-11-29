0280b1854fb7d8f2671824615cc6e988afe33094
julianmack <julianmack@hotmail.co.uk>
2019-11-29 17:33:11 +0000

    Added start of test

--- tests/configs/test_configs.py
+++ tests/configs/test_configs.py
@@ -1,4 +1,5 @@
 import os
+import re
 from glob import glob
 
 import google.protobuf.text_format as text_format  # weird import for mypy
@@ -25,4 +26,17 @@ def test_all_configs_build(config_path):
     """Ensures all `myrtlespeech/config/*.config` files parse."""
     with open(config_path, "r") as config_file:
         config = config_file.read()
+    text_format.Merge(config, task_config_pb2.TaskConfig())
+
+
+def test_model_in_configs_can_be_built(config_path):
+    """Ensures :py:class:`seq_to_seq` in config file can be built.
+
+    Does not build the whole task config as the
+    """
+    with open(config_path, "r") as config_file:
+        config = config_file.read()
+
+    re.split("{|}", config)
+
     text_format.Merge(config, task_config_pb2.TaskConfig())
