b46ed75486072ce70f95ea754841d5a1e8916c44
giuseppeCoccia <giuseppecoccia94@gmail.com>
2019-12-06 10:42:04 +0000

    Add types + change doc syntaxt for hyperlink

--- src/myrtlespeech/data/sampler.py
+++ src/myrtlespeech/data/sampler.py
@@ -1,4 +1,8 @@
 import random
+from typing import Dict
+from typing import Optional
+from typing import Set
+from typing import Union
 
 
 class SequentialRandomSampler:
@@ -15,13 +19,12 @@ class SequentialRandomSampler:
         batch_size: Batch dimension.
         shuffle: Set to True to have the data reshuffled at every epoch if a
             random iterator is used.
-        drop_last: (optional, bool): Set to True to drop the last incomplete
-            batch, if the dataset size is not divisible by the batch size. If
-            False and the size of dataset is not divisible by the batch size,
-            then the last batch will be smaller.
-        n_iterators (optional, int): Number of iterators returned so far.
-        sequential (optional, set of int): Counts at which to return a
-            sequential iterator.
+        drop_last: Set to True to drop the last incomplete batch, if the
+            dataset size is not divisible by the batch size. If False and the
+            size of dataset is not divisible by the batch size, then the last
+            batch will be smaller.
+        n_iterators: Number of iterators returned so far.
+        sequential: Counts at which to return a sequential iterator.
 
     Yields:
         Batches from `batches`.
@@ -29,19 +32,19 @@ class SequentialRandomSampler:
 
     def __init__(
         self,
-        indices,
-        batch_size,
-        shuffle,
-        drop_last=False,
-        n_iterators=0,
-        sequential=None,
+        indices: range,
+        batch_size: int,
+        shuffle: bool,
+        drop_last: Optional[bool] = False,
+        n_iterators: Optional[int] = 0,
+        sequential: Optional[set] = None,
     ):
         self.shuffle = shuffle
         self.batch_indices = self._batch_indices(
             indices, batch_size, drop_last
         )
-        self._n_iterators = n_iterators
-        self._sequential = sequential or {}
+        self._n_iterators: Optional[int] = n_iterators
+        self._sequential: Union[Set, Dict] = sequential or {}
 
     def _batch_indices(self, indices, batch_size, drop_last):
         batches = []
@@ -73,26 +76,30 @@ class SortaGrad(SequentialRandomSampler):
     The SortaGrad curriculum learning strategy iterates over batches from the
     batched dataset sequentially for the first pass and then randomly for all
     other passes. See Deep Speech 2 paper for more information on this:
-    https://arxiv.org/abs/1512.02595
+    `Deep Speech 2 <https://arxiv.org/abs/1512.02595>`_
 
     Args:
         indices: data with which batches are created.
         batch_size: Batch dimension.
         shuffle: Set to True to have the data reshuffled at every epoch if a
             random iterator is used.
-        drop_last: (optional, bool): Set to True to drop the last incomplete
-            batch, if the dataset size is not divisible by the batch size. If
-            False and the size of dataset is not divisible by the batch size,
-            then the last batch will be smaller.
-        start_epoch (optional, int): Number of iterators returned so far by the
-            sampler.
+        drop_last: Set to True to drop the last incomplete batch, if the
+            dataset size is not divisible by the batch size. If False and the
+            size of dataset is not divisible by the batch size, then the last
+            batch will be smaller.
+        start_epoch: Number of iterators returned so far by the sampler.
 
     Yields:
         Batches from `batches`.
     """
 
     def __init__(
-        self, indices, batch_size, shuffle, drop_last=False, start_epoch=0
+        self,
+        indices: range,
+        batch_size: int,
+        shuffle: bool,
+        drop_last: Optional[bool] = False,
+        start_epoch: Optional[int] = 0,
     ):
         super().__init__(
             indices,
