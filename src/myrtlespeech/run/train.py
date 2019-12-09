e60c489685035246c545f6151b01c120eb45eb86
julianmack <julianmack@hotmail.co.uk>
2019-12-09 16:03:46 +0000

    Removed eval() function and reverted to single fit() API

--- src/myrtlespeech/run/train.py
+++ src/myrtlespeech/run/train.py
@@ -12,12 +12,15 @@ from torch.utils.data import DataLoader
 
 def fit(
     seq_to_seq: SeqToSeq,
-    epochs: int,
-    train_loader: DataLoader,
+    epochs: int = 1,
+    train_loader: Optional[DataLoader] = None,
     eval_loader: Optional[DataLoader] = None,
     callbacks: Optional[Collection[Callback]] = None,
 ) -> None:
     r"""Fit ``seq_to_seq`` for ``epochs`` ``train_loader`` iters.
+
+    When ``train_loader=None``, evaluation is performed **once only** with
+    ``eval_loader`` and training is not performed.
 
     Args:
         seq_to_seq: A :py:class:`.SeqToSeq` model.
@@ -25,61 +28,72 @@ def fit(
         epochs: Maximum number of epochs to train ``seq_to_seq`` for. Note that
             the actual number of epochs may be less if
             :py:meth:`.CallbackHandler.on_epoch_end` returns :py:data:`True`.
+            Must be :py:data:`1` if ``train_loader=None``.
 
-        train_loader: A :py:class:`torch.utils.data.DataLoader` for the
-            training data.
+        train_loader: An Optional :py:class:`torch.utils.data.DataLoader` for
+            the training data.
 
         eval_loader: An optional :py:class:`torch.utils.data.DataLoader` for
             the validation data.
 
         callbacks: A collection of :py:class:`.Callback`\s.
     """
+    assert (
+        train_loader or eval_loader
+    ), "Can't have both train_loader==None and eval_loader==None"
+    if train_loader is None:
+        assert epochs == 1, "If train_loader is None, epochs must be 1 "
+
     # sphinx-doc-start-after
     cb_handler = CallbackHandler(callbacks)
     cb_handler.on_train_begin(epochs)
 
     for epoch in range(epochs):
-        stages = [Stage.TRAIN]  # always train for an epoch
+        stages = []
+        if train_loader is not None:
+            if epoch == 0 and eval_loader is not None:
+                stages.append(Stage.EVAL)  # eval before training starts
+            stages.append(Stage.TRAIN)
+
         if eval_loader is not None:
             stages.append(Stage.EVAL)  # eval after every epoch
-            if epoch == 0:
-                stages.insert(0, Stage.EVAL)  # eval before training starts
 
         for stage in stages:
             is_training = stage == Stage.TRAIN
-            loader = train_loader if is_training else eval_loader
-            if run_stage(seq_to_seq, cb_handler, loader, is_training):
-                break
+            seq_to_seq.train(mode=is_training)
+            cb_handler.train(mode=is_training)
 
-    cb_handler.on_train_end()
-    # sphinx-doc-end-before
+            cb_handler.on_epoch_begin()
 
+            with ExitStack() as stack:
+                if not is_training:
+                    stack.enter_context(torch.no_grad())
 
-# sphinx-doc-start-after-run-stage
-def run_stage(seq_to_seq, cb_handler, loader, is_training):
-    seq_to_seq.train(mode=is_training)
-    cb_handler.train(mode=is_training)
+                loader = train_loader if is_training else eval_loader
+                for x, y in loader:
+                    # model
+                    x, y = cb_handler.on_batch_begin(x, y)
+                    out = seq_to_seq.model(x)
 
-    cb_handler.on_epoch_begin()
+                    # loss
+                    loss_out, loss_y = cb_handler.on_loss_begin(out, y)
+                    loss = seq_to_seq.loss(loss_out, loss_y)
+                    loss, skip_bwd = cb_handler.on_backward_begin(loss)
 
-    with ExitStack() as stack:
-        if not is_training:
-            stack.enter_context(torch.no_grad())
+                    # optim
+                    if is_training:
+                        if not skip_bwd:
+                            loss.backward()
 
-        for x, y in loader:
+                        if seq_to_seq.optim is not None:
+                            if not cb_handler.on_backward_end():
+                                seq_to_seq.optim.step()
 
-            x, y = cb_handler.on_batch_begin(x, y)
-            out = seq_to_seq.model(x)
+                            if not cb_handler.on_step_end():
+                                seq_to_seq.optim.zero_grad()
 
-            # loss
-            loss_out, loss_y = cb_handler.on_loss_begin(out, y)
-            loss = seq_to_seq.loss(loss_out, loss_y)
-            loss, skip_bwd = cb_handler.on_backward_begin(loss)
-
-            # optim
-            if is_training:
-                if not skip_bwd:
-                    loss.backward()
+                    if cb_handler.on_batch_end():
+                        break
 
                 if is_training and seq_to_seq.lr_scheduler is not None:
                     seq_to_seq.lr_scheduler.step()
