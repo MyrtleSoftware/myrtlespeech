c9aa8090a1ecf357f053528bfe645daa75b490f5
julianmack <julianmack@hotmail.co.uk>
2020-01-07 11:02:54 +0000

    Added poly lr and updated rnn_t config

--- src/myrtlespeech/lr_scheduler/poly.py
+++ src/myrtlespeech/lr_scheduler/poly.py
@@ -2,14 +2,29 @@ from torch.optim.lr_scheduler import LambdaLR
 
 
 class PolynomialLR(LambdaLR):
-    """A polynomial decay learning rate scheduler."""
+    """A polynomial decay learning rate scheduler.
+
+    Follows the 'poly' decay rate described in `DeepLab: Semantic Image
+    Segmentation with Deep Convolutional Nets, Atrous Convolution,
+    and Fully Connected CRFs <https://arxiv.org/pdf/1606.00915.pdf>`_
+    with a power of 2.
+
+    Args:
+        optimizer: An :py:class:`torch.optim.Optimizer`.
+
+        total_steps: Total number of steps in training. This will be equal to
+            ``steps_per_epoch * epochs``.
+
+        min_lr_multiple: The minimum multiple of the lr.
+
+        last_epoch: The index of the previous epoch.
+    """
 
     def __init__(
         self, optimizer, total_steps, min_lr_multiple=0.01, last_epoch=-1
     ):
         self.total_steps = total_steps
         self.min_lr_multiple = min_lr_multiple
-
         super().__init__(
             optimizer, lr_lambda=self._poly_decay_fn, last_epoch=last_epoch
         )
