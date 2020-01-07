92e439410dc4f900b1a8c471ae9b0067e8fe9491
julianmack <julianmack@hotmail.co.uk>
2020-01-07 09:24:28 +0000

    Added state dict saving

--- src/myrtlespeech/lr/scheduler.py
+++ src/myrtlespeech/lr/scheduler.py
@@ -28,6 +28,34 @@ class _LRSchedulerWarmup(_LRScheduler):
     def _warmup(self, step):
         return min(1, (step / self._num_warmup_steps))
 
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
+
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        state_dict = {
+            key: value
+            for key, value in self.__dict__.items()
+            if key not in ("_optimizer", "_scheduler")
+        }
+
+        state_dict["_scheduler"] = self._scheduler.state_dict()
+
+        return state_dict
+
+    def load_state_dict(self, state_dict):
+        """Loads the schedulers state.
+
+        Arguments:
+            state_dict (dict): scheduler state. Should be an object returned
+                from a call to :meth:`state_dict`.
+        """
+        _scheduler_dict = state_dict.pop("_scheduler")
+
+        self.__dict__.update(state_dict)
+        self._scheduler.__dict__.update(_scheduler_dict)
+
 
 class ConstantLR(LambdaLR):
     def __init__(self, optimizer, last_epoch=-1):
