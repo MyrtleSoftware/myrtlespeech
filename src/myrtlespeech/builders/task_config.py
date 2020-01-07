2104797e22540cf415442ade0f9267c27325d3c9
julianmack <julianmack@hotmail.co.uk>
2020-01-07 09:10:47 +0000

    Added warmup to lr scheduler

--- src/myrtlespeech/builders/task_config.py
+++ src/myrtlespeech/builders/task_config.py
@@ -6,6 +6,8 @@ from myrtlespeech.builders.dataset import build as build_dataset
 from myrtlespeech.builders.speech_to_text import build as build_stt
 from myrtlespeech.data.batch import seq_to_seq_collate_fn
 from myrtlespeech.data.sampler import RandomBatchSampler
+from myrtlespeech.lr.scheduler import _LRSchedulerWarmup
+from myrtlespeech.lr.scheduler import ConstantLR
 from myrtlespeech.model.seq_to_seq import SeqToSeq
 from myrtlespeech.protos import task_config_pb2
 
@@ -91,11 +93,6 @@ def build(
 
     seq_to_seq.optim = optim
 
-    # create learning rate scheduler
-    seq_to_seq.lr_scheduler = _create_lr_scheduler(
-        task_config, seq_to_seq.optim
-    )
-
     # create dataloader
     def target_transform(target):
         return torch.tensor(
@@ -143,6 +140,11 @@ def build(
         pin_memory=torch.cuda.is_available(),
     )
 
+    # create learning rate scheduler
+    seq_to_seq.lr_scheduler = _create_lr_scheduler(
+        task_config, seq_to_seq.optim, len(train_loader)
+    )
+
     return (
         seq_to_seq,
         task_config.train_config.epochs,
@@ -152,8 +154,10 @@ def build(
 
 
 def _create_lr_scheduler(
-    task_config: task_config_pb2.TaskConfig, optimizer: torch.optim
-) -> torch.optim.lr_scheduler:
+    task_config: task_config_pb2.TaskConfig,
+    optimizer: torch.optim,
+    batches_per_epoch: int,
+) -> torch.optim.lr_scheduler._LRScheduler:
     """Builds a ``learning rate scheduler`` and returns it.
 
     Args:
@@ -163,18 +167,20 @@ def _create_lr_scheduler(
         optimizer: A :py:class:`torch.optim` that is used in the learning rate
             scheduler initialization.
 
+        batches_per_epoch: Number of batches in a single epoch.
+
     Returns:
-        A :py:class:`torch.optim.lr_scheduler` instance.
+        A :py:class:`torch.optim.lr_scheduler._LRScheduler` instance.
 
     Raises:
         :py:class:`ValueError`: On invalid configuration.
-        """
+    """
     lr_scheduler_str = task_config.train_config.WhichOneof(
         "supported_lr_scheduler"
     )
 
     if lr_scheduler_str == "constant_lr":
-        lr_scheduler = None
+        lr_scheduler = ConstantLR(optimizer=optimizer)
     elif lr_scheduler_str == "step_lr":
         kwargs = {}
 
@@ -205,6 +211,25 @@ def _create_lr_scheduler(
     else:
         raise ValueError(
             f"unsupported learning rate scheduler {lr_scheduler_str}"
+        )
+
+    # get scheduler step frequency
+    if lr_scheduler_str in [
+        "step_lr",
+        "exponential_lr",
+        "cosine_annealing_lr",
+    ]:
+        step_freq = batches_per_epoch  # Step at end of each epoch
+    elif lr_scheduler_str in []:
+        step_freq = 1  # Step after every batch
+    elif lr_scheduler_str == "constant_lr":
+        step_freq = batches_per_epoch * 100  # Arbitrary large value
+
+    # Maybe add lr warmup
+    if task_config.train_config.HasField("lr_warmup"):
+        warmup_cfg = task_config.train_config.lr_warmup
+        lr_scheduler = _LRSchedulerWarmup(
+            lr_scheduler, step_freq, warmup_cfg.num_warmup_steps
         )
 
     return lr_scheduler
