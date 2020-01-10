9e277351a94a2bd8be23821dda52fbfd3c5e7db8
julianmack <julianmack@hotmail.co.uk>
2020-01-10 14:50:40 +0000

    Started implementing speed_pertubation

--- src/myrtlespeech/data/augment.py
+++ src/myrtlespeech/data/augment.py
@@ -3,6 +3,66 @@ import random
 import torch
 
 
+# class SpeedPertubation:
+#     """Applies Speed Pertubation.
+#
+#    Applies Speed Pertubation as in `Audio Augmentation for Speech Recognition
+#     <https://www.danielpovey.com/files/2015_interspeech_augmentation.pdf>`_.
+#     Note that this alters the length of the output signal.
+#
+#     Args:
+#        max_perturb_rate_diff: A float that gives the maximum difference ratio
+#             between the original speed and the perturbed speed. For example,
+#             if ``max_perturb_rate_diff = 0.15``, the augmented waveform
+#            will have a speed in the range [0.85, 1.15] of the original speed.
+#
+#     Raises:
+#         :py:class:`ValueError`: if ``max_perturb_rate_diff > 1.``.
+#     """
+#
+#     def __init__(
+#         self, max_perturb_rate_diff: float = 0.15,
+#     ):
+#         if max_perturb_rate_diff >= 1.0:
+#             raise ValueError("max_perturb_rate_diff cannot be >= 1.")
+#         self.min_rate = 1.0 - max_perturb_rate_diff
+#         self.max_rate = 1.0 + max_perturb_rate_diff
+#
+#     def __call__(self, waveform: torch.Tensor) -> torch.Tensor:
+#         """Returns ``waveform`` after applying SpeedPertubation.
+#
+#         Args:
+#             waveform: :py:class:`torch.Tensor` with size TODO
+#
+#         Returns:
+#             :py:class:`torch.Tensor` with size TODO
+#         """
+#         _, n_features, n_time_steps = x.size()
+#
+#         # mask features
+#         for _ in range(self.n_feature_masks):
+#             f_to_mask = random.randint(0, self.feature_mask)
+#             f_start = random.randint(0, max(0, n_features - f_to_mask))
+#             x[:, f_start : f_start + f_to_mask, :] = 0
+#
+#         # mask time steps
+#         for _ in range(self.n_time_masks):
+#             t_to_mask = random.randint(0, self.time_mask)
+#             t_start = random.randint(0, max(0, n_time_steps - t_to_mask))
+#             x[:, :, t_start : t_start + t_to_mask] = 0
+#
+#         return x
+#
+#     def __repr__(self) -> str:
+#         return (
+#             self.__class__.__name__
+#             + f"(feature_mask={self.feature_mask},"
+#             + f" time_mask={self.time_mask},"
+#             + f" n_feature_masks={self.n_feature_masks},"
+#             + f" n_time_masks={self.n_time_masks})"
+#         )
+
+
 class SpecAugment:
     """`SpecAugment <https://arxiv.org/pdf/1904.08779.pdf>`_.
 
