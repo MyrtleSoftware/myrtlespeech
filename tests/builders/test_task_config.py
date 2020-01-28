30d191146da8275c0198dc9efff804d80ca8525f
julianmack <julianmack@hotmail.co.uk>
2020-01-28 10:58:22 +0000

    Removed hypothesis deadline

--- tests/builders/test_task_config.py
+++ tests/builders/test_task_config.py
@@ -2,7 +2,6 @@ import warnings
 
 import torch
 from hypothesis import given
-from hypothesis import settings
 from myrtlespeech.builders.task_config import build
 from myrtlespeech.protos import task_config_pb2
 
@@ -13,7 +12,6 @@ from tests.protos.test_task_config import task_configs
 
 
 @given(task_cfg=task_configs())
-@settings(deadline=3000)
 def test_build_returns(task_cfg: task_config_pb2.TaskConfig) -> None:
     """Test that build returns when called."""
     model, epochs, train_loader, eval_loader = build(task_cfg)
